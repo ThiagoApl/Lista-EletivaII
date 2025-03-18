fbb5fc59ae421a9aa18318b66eb2a74b37543028
Thiago Augustus Palacio <162067956+ThiagoApl@users.noreply.github.com>
2025-03-18 20:52:01 -0300

    Create formulario.blade.php

---
+++ primeiralista/resources/views/formulario.blade.php
@@ -0,0 +1,19 @@
+@extends ('Layout')
+@section('Title', 'Primeiro Exemplo')
+@section('content')
+    <h1 class="text-center"> Formulario de Valores</h1>
+    <form action="/formulario_resposta" method="POST"></form>
+        @csrf
+        <div class="mb-3">
+            <label for="valor1" class="form-label"> Valor 1</label>
+            <input type="number" class="form-control" id="valor1" name="valor1"
+                placeholder="Digite o Primeiro Valor" required>
+        </div>
+        <div class="mb-3">
+            <label for="valor2" class="form-label"> Valor2</label>
+            <input type="number" class="form-control" id="valor2" name="valor2"
+                placeholder="Digite o segundo valor" required>
+        </div>
+        <button type="submit" class="btn btn-primary"> Enviar</button>
+    </form>
+@endsection
