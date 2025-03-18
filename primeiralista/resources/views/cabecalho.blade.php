242f29a17884edab6973a2c5d45c559b675b7251
Thiago Augustus Palacio <162067956+ThiagoApl@users.noreply.github.com>
2025-03-18 20:38:36 -0300

    Create cabecalho.blade.php

---
+++ primeiralista/resources/views/cabecalho.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">App Laravel</a>
+        <button class="navbar-toogler" type="button" data-bs-toogler="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"aria-label="Toogle navigation">
+            <span class="navbar-toogler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/formulario"> Primeiro Exemplo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"> Exercicio 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Exercicio 2</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
