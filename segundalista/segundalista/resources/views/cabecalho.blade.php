3f547ae6f35d54e1df1747248fb4851ba16a82f1
Thiago Augustus Palacio <thiago.luppi@fatec.sp.gov.br>
2025-04-08 23:39:58 -0300

    ex1 ex2 ex3

---
+++ segundalista/segundalista/resources/views/cabecalho.blade.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">LARAVEL</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Exercícios Lista 2
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/ex1">Soma dos valores</a></li>
+            <li><a class="dropdown-item" href="/ex2">Valores A e B</a></li>
+            <li><a class="dropdown-item" href="/ex3">Desconto > R$ 100,00</a></li>
+            <li><a class="dropdown-item" href="/ex4">Valores primos</a></li>
+            <li><a class="dropdown-item" href="/ex5">Mês correspondente</a></li>
+            <li><a class="dropdown-item" href="/ex6">Imprimir números</a></li>
+            <li><a class="dropdown-item" href="/ex7">Somar números</a></li>
+            <li><a class="dropdown-item" href="/ex8">Contagem regressiva</a></li>
+            <li><a class="dropdown-item" href="/ex9">Calcular o fatorial</a></li>
+            <li><a class="dropdown-item" href="/ex10">Tabuada</a></li>
+            
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
