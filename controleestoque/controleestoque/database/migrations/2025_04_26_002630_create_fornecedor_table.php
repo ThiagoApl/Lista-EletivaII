97f801ade4c8b58fac19181c968fac21c4794fbd
Thiago Augustus Palacio <thiago.luppi@fatec.sp.gov.br>
2025-04-27 20:37:40 -0300

    Update 2025_04_26_002630_create_fornecedor_table.php

--- controleestoque/controleestoque/database/migrations/2025_04_26_002630_create_fornecedor_table.php
+++ controleestoque/controleestoque/database/migrations/2025_04_26_002630_create_fornecedor_table.php
@@ -1,7 +1,5 @@
 <?php
-use Illuminate\Database\Migrations\Migration; 
-use Illuminate\Database\Schema\Blueprint; 
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration; use Illuminate\Database\Schema\Blueprint; use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
@@ -9,7 +7,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('fornecedor', function (Blueprint $table) {
+        Schema::create('produtos', function (Blueprint $table) {
             $table->id();
             $table->string('nome');
             $table->string('foto')->nullable(); // Pode ser nulo
