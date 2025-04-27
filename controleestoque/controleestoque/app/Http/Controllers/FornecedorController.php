1172c2b588bf7ea88dc5b87c60712d7dc9f7af6c
Thiago Augustus Palacio <thiago.luppi@fatec.sp.gov.br>
2025-04-27 20:23:00 -0300

    update

--- controleestoque/controleestoque/app/Http/Controllers/FornecedorController.php
+++ controleestoque/controleestoque/app/Http/Controllers/FornecedorController.php
@@ -2,9 +2,74 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+    use App\Models\Fornecedor;
+    use Illuminate\Http\Request;
 
-class FornecedorController extends Controller
-{
-    //
-}
+    class FornecedorController extends Controller
+    {
+        /**
+         * Display a listing of the resource.
+         */
+        public function index()
+        {
+            $fornecedor = Fornecedor::all();
+            return view('fornecedor.index', compact('fornecedor'));
+        }
+
+        /**
+         * Show the form for creating a new resource.
+         */
+        public function create()
+        {
+            return view('fornecedor.create');
+        }
+
+        /**
+         * Store a newly created resource in storage.
+         */
+        public function store(Request $request)
+        {
+            $request->validate([
+                'nome' => 'required',
+                'codigo' => 'required|unique:produtos',
+            ]);
+
+            Fornecedor::create($request->all());
+
+            return redirect()->route('fornecedor.index')->with('success', 'fornecedor criado com sucesso!');
+        }
+
+        /**
+         * Show the form for editing the specified resource.
+         */
+        public function edit(Fornecedor $fornecedor)
+        {
+            return view('fornecedor.edit', compact('fornecedor'));
+        }
+
+        /**
+         * Update the specified resource in storage.
+         */
+        public function update(Request $request, Fornecedor $fornecedor)
+        {
+            $request->validate([
+                'nome' => 'required',
+                'codigo' => 'required|unique:produtos,codigo,' . $fornecedor->id,
+            ]);
+
+            $fornecedor->update($request->all());
+
+            return redirect()->route('fornecedor.index')->with('success', 'fornecedor atualizado com sucesso!');
+        }
+
+        /**
+         * Remove the specified resource from storage.
+         */
+        public function destroy(Fornecedor $fornecedor)
+        {
+            $fornecedor->delete();
+
+            return redirect()->route('fornecedor.index')->with('success', 'fornecedor excluído com sucesso!');
+        }
+    }
+
--- controleestoque/controleestoque/app/Models/Fornecedor.php
+++ controleestoque/controleestoque/app/Models/Fornecedor.php
@@ -1,73 +1,15 @@
-<?php
+<? php
+namespace App\Models;
 
-namespace App\Http\Controllers;
-use App\Models\Fornecedor;
-use Illuminate\Http\Request;
-
-class FornecedorController extends Controller
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Fornecedor extends Model
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $produtos = Fornecedor::all();
-        return view('fornecedor.index', compact('fornecedor'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('fornecedor.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'nome' => 'required',
-            'codigo' => 'required|unique:fornecedor',
-        ]);
-
-        Fornecedor::create($request->all());
-
-        return redirect()->route('fornecedor.index')->with('success', 'fornecedor criado com sucesso!');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Fornecedor $fornecedor)
-    {
-        return view('fornecedor.edit', compact('fornecedor'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Fornecedor $fornecedor)
-    {
-        $request->validate([
-            'nome' => 'required',
-            'codigo' => 'required|unique:fornecedor,codigo,' . $fornecedor->id,
-        ]);
-
-        $fornecedor->update($request->all());
-
-        return redirect()->route('fornecedor.index')->with('success', 'fornecedor atualizado com sucesso!');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Fornecedor $fornecedor)
-    {
-        $produto->delete();
-
-        return redirect()->route('fornecedor.index')->with('success', 'fornecedor excluído com sucesso!');
-    }
+    use HasFactory;
+    protected $fillable = [
+        'nome',
+        'foto',
+        'codigo',
+        'quantidade_funcionarios_acesso'
+    ];
 }
