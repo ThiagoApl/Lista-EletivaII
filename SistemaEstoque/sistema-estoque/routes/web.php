a9439ebfc5dd70a55968d0a31dbc8c6e63b8a486
ThiagoApl <thiago.luppi@fatec.sp.gov.br>
2025-04-30 19:33:55 -0300

    Update web.php

--- SistemaEstoque/sistema-estoque/routes/web.php
+++ SistemaEstoque/sistema-estoque/routes/web.php
@@ -3,14 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get ('/', function () {
-    return view('Welcome');
+    return view ('Welcome');
 });
 
 Route::get ('/formulario', function () {
     return view('formulario');
 });
 
-Route::post('/formulario_resposta', function(\Illuminate\Http\Request $request) {
+Route::post('/formulario_resposta', function (\Illuminate\Http\Request $request) {
     $valor1 = intval($request->input('Valor1'));
     $valor2 = intval($request->input('Valor2'));
     $soma = $valor1 + $valor2;
