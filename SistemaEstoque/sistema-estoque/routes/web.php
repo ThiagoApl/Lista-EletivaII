3c673615817046e41fa90db66d045cbd856cfa90
ThiagoApl <thiago.luppi@fatec.sp.gov.br>
2025-04-30 19:23:31 -0300

    Update web.php

--- SistemaEstoque/sistema-estoque/routes/web.php
+++ SistemaEstoque/sistema-estoque/routes/web.php
@@ -2,17 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get ('/', function () {
+    return view('Welcome');
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get ('/formulario', function () {
+    return view('formulario');
+});
+
+Route::post('/formulario_resposta', function(\Illuminate\Http\Request $request) {
+    $valor1 = intval($request->input('Valor1'));
+    $valor2 = intval($request->input('Valor2'));
+    $soma = $valor1 + $valor2;
+    return view('formulario', compact('soma'));
 });
