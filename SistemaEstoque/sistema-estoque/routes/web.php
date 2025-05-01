39b4ac186ce9c7702e7f8b007aced8bd2d8604bd
Thiago Augustus Palacio <thiago.luppi@fatec.sp.gov.br>
2025-05-01 20:46:32 -0300

    controller route

--- SistemaEstoque/sistema-estoque/routes/web.php
+++ SistemaEstoque/sistema-estoque/routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\VendedorController;
+use App\Http\Controllers\EntradaController;
+use App\Http\Controllers\SaidaController;
 
-Route::get ('/', function () {
-    return view ('Welcome');
-});
-
-Route::get ('/formulario', function () {
-    return view('formulario');
-});
-
-Route::post('/formulario_resposta', function (\Illuminate\Http\Request $request) {
-    $valor1 = intval($request->input('Valor1'));
-    $valor2 = intval($request->input('Valor2'));
-    $soma = $valor1 + $valor2;
-    return view('formulario', compact('soma'));
-});
+Route::resource('produtos', ProdutoController::class);
+Route::resource('fornecedores', FornecedorController::class);
+Route::resource('vendedores', VendedorController::class);
+Route::resource('entradas', EntradaController::class);
+Route::resource('saidas', SaidaController::class);
