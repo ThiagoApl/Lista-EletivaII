e756903e93f87d8520c72bdef7da0e9803bbf814
Thiago Augustus Palacio <162067956+ThiagoApl@users.noreply.github.com>
2025-06-16 21:09:25 -0300

    Delete storage directory

--- storage/framework/views/ca284c891e363053e503aadb12a3e173.php
+++
@@ -1,9 +0,0 @@
-
-
-<?php $__env->startSection('title', 'Início'); ?>
-
-<?php $__env->startSection('content'); ?>
-    <h1 class="text-2xl font-bold mb-4">Bem-vindo ao sistema de emissão de etiquetas</h1>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.admin', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\User\Desktop\Projeto-Thiago\resources\views/home.blade.php ENDPATH**/ ?>
