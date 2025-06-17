1f26eab53812ab8d10d1dfa3206ea3d034b52cc7
Thiago Augustus Palacio <162067956+ThiagoApl@users.noreply.github.com>
2025-06-16 21:09:04 -0300

    Delete resources directory

--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.admin')
-
-@section('title', 'Início')
-
-@section('content')
-    <h1 class="text-2xl font-bold mb-4">Bem-vindo ao sistema de emissão de etiquetas</h1>
-@endsection
